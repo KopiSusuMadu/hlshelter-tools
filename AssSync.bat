ed970e5c6fe1dbe2163e1a1ad8b23655a94ae8e9
AnggaraNothing <anggaranothing.other228@simplelogin.com>
2024-02-06 00:04:49 +0700

    Initial commit

---
+++ AssSync.bat
@@ -0,0 +1,6 @@
+dotnet "./bin/AssetSynchronizer.dll"^
+	--assets-directory "../hlshelter-assets"^
+	--mod-directory "../hlshelter-mod/hlshelter"^
+	--asset-manifest "../hlshelter-assets/AssetManifest.json"
+
+pause
